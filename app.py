3b0168f1a9383356961a01c0131d7bbefd7ac777
Simeone <simeoneumoh@gmail.com>
2019-09-29 22:37:11 +0100

    Created Database and added configuration files

--- app.py
+++ app.py
@@ -1,2 +1,30 @@
 import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash 
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+DATABASE = '/home/simeone/Documents/Blog/blog.db'
+DEBUG = True
+SECRET_KEY = 'secret key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+def connect_db():
+    sql = sqlite3.connect('/home/simeone/Documents/Blog/blog.db')
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db():
+    if not hasattr(g, 'sqlite3_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run()
