84263295fc9e7194a98d05c8294bb13856b3d6d6
Simeone <simeoneumoh@gmail.com>
2019-10-01 08:13:52 +0100

    Final Commit

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, g
 
-DATABASE = '/home/simeone/Documents/Blog/blog.db'
-DEBUG = True
-SECRET_KEY = 'secret key'
-USERNAME = 'admin'
-PASSWORD = 'default'
+app = Flask(__name__)
+app.config['USERNAME'] = 'simeon'
+app.config['PASSWORD'] = 'default'
+app.config['SECRET_KEY'] = 'Secretkey'
 
 def connect_db():
     sql = sqlite3.connect('/home/simeone/Documents/Blog/blog.db')
@@ -24,18 +23,20 @@ def close_db(error):
         g.sqlite_db.close()
 
 
-app = Flask(__name__)
+
 
 
 @app.route('/')
 def show_entries():
+    db = get_db()
     cur = db.execute('select title,text from entries order by id desc')
     entries = [dict (title = row[0], text = row [1]) for row in cur.fetchall ()]
 
-    return render_template('show_entries.html, entries')
+    return render_template('show_entries.html', entries=entries)
 
 @app.route('/add', methods = ['POST'])
 def add_entry():
+    db = get_db()
     if not session.get('logged_in'):
         abort(401)
     db.execute ('insert into entries (title,text) values(?,?)', [request.form['title'], request.form['text']])
@@ -46,6 +47,7 @@ def add_entry():
 
 @app.route('/login' , methods =['GET','POST'])
 def login():
+    
     error = None
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
@@ -54,7 +56,7 @@ def login():
             error = 'Invalid Password'
         else:
             session['logged_in'] = True
-            flash*('you were logged in')
+            flash('you were logged in')
             return redirect(url_for('show_entries'))
     return render_template('login.html', error = error)
 
