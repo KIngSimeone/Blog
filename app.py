4410c2c6e1a214ea91cf2a5716a49e01fdae5498
Simeone <simeoneumoh@gmail.com>
2019-10-01 06:30:57 +0100

    Added Login View function

--- app.py
+++ app.py
@@ -44,5 +44,20 @@ def add_entry():
 
     return redirect( url_for('show_entries'))
 
+@app.route('/login' , methods =['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = "Invalid Username"
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Password'
+        else:
+            session['logged_in'] = True
+            flash*('you were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error = error)
+
+
 if __name__ == '__main__':
     app.run()
